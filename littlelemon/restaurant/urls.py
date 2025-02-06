8e77f6ec764e23376979bbceb63a110fe8f0e5f1
krishnasingh281 <samar.phone.home@gmail.com>
2025-02-06 15:31:25 +0530

    created serializer.py

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # path('', views.sayHello, name = 'sayHello'),
     path('', views.index, name='index'),
+    # path('menu/', views.menu)
+    path('booking/', views.BookingSerializer, name = 'booking'),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Booking, Menu
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 
@@ -9,3 +13,18 @@ def sayHello(request):
 
 def index(request):
     return render(request, 'index.html', {})
+
+class bookingView(APIView):
+    
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many = True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = MenuSerializer(data = request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status" : "success",  "data" : serializer.data})
+            
